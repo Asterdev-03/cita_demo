403ea9df1df99ee44178365a7d75cc9058c7e866
Augustin C Benny <augustincbenny2001@gmail.com>
2023-11-24 09:29:49 +0530

    Fix: Bug fixed for microphone

--- app/(user_pages)/mock-test/page.jsx
+++ app/(user_pages)/mock-test/page.jsx
@@ -90,6 +90,7 @@ export default function MockTestPage() {
   }
 
   function handleSend() {
+    SpeechRecognition.stopListening();
     resetTranscript();
     setVoiceStatus(false);
   }
@@ -227,45 +228,3 @@ export default function MockTestPage() {
     </section>
   );
 }
-
-// export default function MockTestPage() {
-//   const webcamRef = useRef(null);
-//   const [videoStream, setVideoStream] = useState(null);
-
-//   useEffect(() => {
-//     startVideoStream();
-//   }, []);
-
-//   async function startVideoStream() {
-//     if (
-//       typeof webcamRef.current !== "undefined" &&
-//       webcamRef.current !== null &&
-//       webcamRef.current.video.readyState === 4
-//     ) {
-//       const video = webcamRef.current.video;
-//       const videoWidth = webcamRef.current.video.videoWidth;
-//       const videoHeight = webcamRef.current.video.videoHeight;
-
-//       // Set video to be displayed based on the actual width and height of the direct video from camera
-//       webcamRef.current.video.width = videoWidth;
-//       webcamRef.current.video.height = videoHeight;
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <Webcam
-//         ref={webcamRef}
-//         muted={true} // this mutes the audio feed
-//         audio={false}
-//         style={{
-//           position: "absolute",
-//           top: "50%",
-//           left: "50%",
-//           transform: "translate(-50%, -50%)",
-//         }}
-//       />
-//       {/* {videoStream && <video srcObject={videoStream} autoPlay />} */}
-//     </div>
-//   );
-// }
